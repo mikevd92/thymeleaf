dd61213b494d7c2fe25e8091bb7610b66cb6ab97
misuvd92 <misuvd92@yahoo.com>
2014-08-26 12:53:37 +0200

    changed some whitespaces

--- mavenproject1/src/main/java/controller/LoginController.java
+++ mavenproject1/src/main/java/controller/LoginController.java
@@ -5,8 +5,6 @@
  */
 package controller;
 
-import javax.servlet.http.HttpServletRequest;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
