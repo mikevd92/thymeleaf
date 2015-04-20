c51f9bd6d4eb1421ae311a76bd605f8d6920f879
misuvd92 <misuvd92@yahoo.com>
2015-04-20 12:24:35 +0300

    added row highlighting

--- mavenproject1/src/main/java/service/UtilsImpl.java
+++ mavenproject1/src/main/java/service/UtilsImpl.java
@@ -44,7 +44,7 @@ public class UtilsImpl implements Utils {
                     + "			<th>Price</th>\n"
                     + "                    </tr>\n"
                     + "                </thead>\n"
-                    + "                <tbody>\n";
+                    + "                <tbody id=\"body\">\n";
             body += opt.get();
             body += "                </tbody>\n            </table>";
         }
